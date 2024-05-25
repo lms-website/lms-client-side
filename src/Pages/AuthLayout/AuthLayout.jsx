684e73f1e5e8549242905f62388191cb82d69a82
Mariam_Tarek <mariamelsarag44@gmail.com>
2024-05-26 01:45:10 +0300

    sidebar

--- src/Pages/AuthLayout/AuthLayout.jsx
+++ src/Pages/AuthLayout/AuthLayout.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Sidebar from "../../Ui/Sidebar";
+import { Outlet } from "react-router-dom";
 
 const AuthLayout = () => {
-  return <div>auth</div>;
+  return (
+    <div className=" h-screen bg-body flex gap-4">
+      <Sidebar />
+      <div className="h-full  flex-1 overflow-y-auto py-4">
+        <Outlet />
+      </div>
+    </div>
+  );
 };
 
 export default AuthLayout;
